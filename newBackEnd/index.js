5e07a16b9fc85a53d5fdee13a0c00251fdff0bbd
alanldias <alan.dias.pe@gmail.com>
2022-05-25 17:03:09 -0300

    basic auth

--- newBackEnd/index.js
+++ newBackEnd/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const basicAuth = require('express-basic-auth')
 const app = express()
 const port = 3000
+
+app.use(basicAuth({
+  users: { 'admin': 'supersecret' }
+}))
+
+app.use(function (req, res, next) {
+  console.log('middleware 1')
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send('Teste!')
